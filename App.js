2c30c63033545df1b0ecd8fcaf8d8624e4bd23f3
Hariharan Kailas <hariharan.ramesh@ymedialabscom.onmicrosoft.com>
2022-11-28 14:43:38 +0530

    replaced auth token

--- App.js
+++ App.js
@@ -20,7 +20,6 @@ export default function App() {
 
   async function fetchRandomUser() {
     const users = await getRandomUser();
-    console.log("hh", users);
     setUsersList(users.data);
   }
 
